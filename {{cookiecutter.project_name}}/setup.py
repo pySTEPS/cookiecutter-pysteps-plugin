6668e572ea1469411af7416f944b9388904c50a1
Andres Perez Hortal <aperezhortal@gmail.com>
2020-10-12 22:07:00 -0400

    Improve entry-points description

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -49,13 +49,36 @@ setup(
     name='{{ cookiecutter.project_name }}',
     packages=find_packages(),
     setup_requires=setup_requirements,
-    # In the entry points below, add the importers included in this package.
-    # entry_points: dictionary mapping entry point group names to a list of strings defining the plugins provided by this package.
-    # The entry point for pysteps importers is "pysteps.plugins.importers".
+    # Entry points
+    # ~~~~~~~~~~~~
+    #
+    # This is the most important part of the plugin setup script.
+    # Entry points are a mechanism for an installed python distribution to advertise
+    # some of the components installed (packages, modules, and scripts) to other
+    # applications (in our case, pysteps).
+    # https://packaging.python.org/specifications/entry-points/
+    #
+    # An entry point is defined by three properties:
+    # - The group that an entry point belongs indicate the kind of functionality that
+    #   provides. For the pysteps importers use the "pysteps.plugins.importers" group.
+    # - The unique name that is used to identify this entry point in the
+    #   "pysteps.plugins.importers" group.
+    # - A reference to a Python object. For the pysteps importers, the object should
+    #   point to a importer function, and should have the following form:
+    #   package_name.module:function.
+    # The setup script uses a dictionary mapping the entry point group names to a list
+    # of strings defining the importers provided by this package (our plugin).
+    # The general form of the entry points dictionary is:
+    # entry_points={
+    #     "group_name": [
+    #         "entry_point_name=package_name.module:function",
+    #         "entry_point_name=package_name.module:function2",
+    #     ]
+    # },
     entry_points={
         'pysteps.plugins.importers': [
-            'import_abc_yyy={{ cookiecutter.project_slug }}:{{ cookiecutter.importer_name }}.import_abc_yyy',
-            'import_abc_zzz={{ cookiecutter.project_slug}}:{{ cookiecutter.importer_name }}.import_abc_zzz',
+            'import_abc_yyy={{ cookiecutter.project_slug }}.{{ cookiecutter.importer_name }}:import_abc_yyy',
+            'import_abc_zzz={{ cookiecutter.project_slug}}.{{ cookiecutter.importer_name }}:import_abc_zzz',
         ]
     },
     version='{{ cookiecutter.version }}',
