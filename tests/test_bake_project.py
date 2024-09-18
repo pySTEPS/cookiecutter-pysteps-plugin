8741b02c11e61932d2eea323f8e7f07a8fae13bb
joeycasey87 <139773651+joeycasey87@users.noreply.github.com>
2024-09-18 13:34:17 +0200

    Update test_bake_project.py

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -80,7 +80,7 @@ def test_bake_with_defaults(cookies):
 
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert "setup.py" in found_toplevel_files
-        assert "pysteps_importer_abc" in found_toplevel_files
+        assert "pysteps_importer_institution_name" in found_toplevel_files
         assert "tox.ini" in found_toplevel_files
         assert "tests" in found_toplevel_files
 
