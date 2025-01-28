bf759dff9751bf358a0fb8b62f7e2fefad618e2a
Lesley De Cruz <lesley.decruz@meteo.be>
2025-01-28 17:30:12 +0100

    Update docs

--- docs/diagnostics_module_example.py
+++ docs/diagnostics_module_example.py
@@ -6,17 +6,17 @@ This is a more extensive description (optional) that describes the readers imple
 in this module and other relevant information.
 """
 
-# Function {{ cookiecutter.plugin_name }} to create diagnostic postprocessing plugins.
+# Function {{ cookiecutter.plugin_name }} to create diagnostic plugins.
 
-# IMPORTANT: The name of the diagnostic postprocessor should follow the "diagnostic_name"
-# naming convention. The "diagnostics_" prefix to the diagnostic postprocessor name is MANDATORY since it is
+# IMPORTANT: The name of the diagnostic should follow the "diagnostic_name"
+# naming convention. The "diagnostics_" prefix to the diagnostic name is MANDATORY since it is
 # used by the pysteps interface.
 #
-# Check the pysteps documentation for examples of diagnostic postprocessor names that follow this
+# Check the pysteps documentation for examples of diagnostic names that follow this
 # convention:
 # https://pysteps.readthedocs.io/en/latest/pysteps_reference/io.html#available-diagnostics
 #
-# The function prototype for the diagnostic postprocessor's declaration should have the following form:
+# The function prototype for the diagnostic's declaration should have the following form:
 #
 #  def diagnostics_xyz(filename, **kwargs):
 #
@@ -27,7 +27,7 @@ in this module and other relevant information.
 # The function arguments should have the following form:
 # (filename, keyword1="some_keyword", keyword2=10,...,keywordN="something", **kwargs)
 # The `filename` and `**kwargs` arguments are mandatory to comply with the pysteps
-# interface. To fine-control the behavior of the diagnostic postprocessor, additional keywords can be
+# interface. To fine-control the behavior of the diagnostic, additional keywords can be
 # added to the function.
 # For example: keyword1="some_keyword", keyword2=10, ..., keywordN="something"
 # It is recommended to declare the keywords explicitly in the function to improve the
@@ -37,15 +37,15 @@ in this module and other relevant information.
 # Return arguments
 # ~~~~~~~~~~~~~~~~
 #
-# The diagnostic postprocessor can return whatever argument is needed.
+# The diagnostic can return whatever argument is needed.
 #
 #
 
 
-def postprocessor_diagnostics_calc(filename, **kwargs):
+def diagnostics_calc(filename, **kwargs):
     """
-    A detailed description of the diagnostic postprocessor. A minimal documentation is
-    strictly needed since the pysteps postprocessor interface expect docstrings.
+    A detailed description of the diagnostic. A minimal documentation is
+    strictly needed since the pysteps postprocessing interface expect docstrings.
 
     For example, a documentation may look like this:
 
@@ -57,10 +57,10 @@ def postprocessor_diagnostics_calc(filename, **kwargs):
           Name of the file to be processed.
 
       keyword1 : str
-          Some keyword used to fine control the diagnostic postprocessor behaviour.
+          Some keyword used to fine control the diagnostic behaviour.
 
       keyword2 : int
-          Another keyword used to fine control the diagnostic postprocessor behaviour.
+          Another keyword used to fine control the diagnostic behaviour.
 
       {extra_kwargs_doc}
 
@@ -68,7 +68,7 @@ def postprocessor_diagnostics_calc(filename, **kwargs):
     -------.
     """
     ####################################################################################
-    # Add the code required to run the diagnostic postprocessor here.
+    # Add the code required to run the diagnostics here.
     file = open(filename, "w")
     file.write("hello world")
     file.close()
